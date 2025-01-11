c17906790219c5c177db82ef2bd6b6342d23c881
Shagan742 <526ssangha@frhsd.com>
2025-01-11 02:08:03 +0000

    the options finally work....

--- script.js
+++ script.js
@@ -1,13 +1,61 @@
 // user options
 
-function choice1Clicked() {
-  const userChooseRock=document.getElementById('choice1');
-// let imageChoice='';
+function choiceClicked() {
+ 
+const container=document.getElementById('userChoicePutSmallerScreen');
+const containerTwo=document.getElementById('userChoicePutLargerScreen');
+let htmlImage='';
 
-//   userChooseRock.addEventListener('click', function() {
-//     document.getElementById('userChoicePutSmallerScreen');
-    
-//   })
+if(document.activeElement.id==='choice1') {
+  htmlImage='<img src="imgs/pngegg.png">';
+} else if (document.activeElement.id==='choice2') {
+  htmlImage='<img src="imgs/paper.png">';
+} else {
+  htmlImage='<img src="imgs/scissors.png">';
+}
+
+container.innerHTML=htmlImage;
+
+if(document.activeElement.id==='choice1') {
+  htmlImage='<img src="imgs/pngegg.png">';
+  
+
+} else if (document.activeElement.id==='choice2') {
+  htmlImage='<img src="imgs/paper.png">';
+
+
+} else {
+  htmlImage='<img src="imgs/scissors.png">';
+
+
+}
+
+containerTwo.innerHTML=htmlImage;
+
+}
+
+function genComputerChoice() {
+
+  const computerChoiceContainer=document.getElementById('computerChoicePutSmallerScreen');
+  const randomNumber=Math.floor(Math.random()*3);
+
+  const computerChoiceContainerTwo=document.getElementById('computerChoicePutLargerScreen');
+
+  if(randomNumber===0) {
+    computerChoiceContainer.innerHTML='<img src="imgs/pngegg.png">';
+  } else if (randomNumber===1) {
+    computerChoiceContainer.innerHTML='<img src="imgs/scissors.png">';
+  } else {
+    computerChoiceContainer.innerHTML='<img src="imgs/paper.png">';
+  }
+
+  if(randomNumber===0) {
+    computerChoiceContainerTwo.innerHTML='<img src="imgs/pngegg.png">';
+  } else if (randomNumber===1) {
+    computerChoiceContainerTwo.innerHTML='<img src="imgs/scissors.png">';
+  } else {
+    computerChoiceContainerTwo.innerHTML='<img src="imgs/paper.png">';
+  }
 
 }
 
